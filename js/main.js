d3131fdeb865b89d17d20ad40378739eaef4a5a7
mily rivas <mrise.translations@gmail.com>
2017-07-13 22:35:53 -0400

    probando el input saldo en consola

--- js/main.js
+++ js/main.js
@@ -1,6 +1,26 @@
 /* Holiii acá va tu código también */
+$(document).ready(function(){
+  
+  var numero_tarjeta;
+    $("#input-numero-tarjeta").keyup(function (e) { 
+        
+        numero_tarjeta = $("#input-numero-tarjeta").val();
 
-  // Show sideNav
+            
+    });
+  
+    $("#button-ver-saldo").on('click',function(){
+       
+       console.log(numero_tarjeta);
+        
+    });
+   
+    
+});
+ 
+
+
+  //MENU HAMBURGUESA
   $('.button-collapse').sideNav();
   // Hide sideNav
   
