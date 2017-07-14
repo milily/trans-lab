c31aa6deb71324951184145d7ec24b63e4db1802
mily rivas <mrise.translations@gmail.com>
2017-07-14 11:42:09 -0400

    validación de formulario

--- js/main.js
+++ js/main.js
@@ -1,23 +1,52 @@
 /* Holiii acá va tu código también */
 $(document).ready(function(){
-  
+
+	var valEmail = /^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/;
+	    $("#btn-inicio-sesion").click(function (){
+	        if( $("#input.email").val() == "" || !valEmail.test($("#input-email").val()) ){
+	            $("#input-email").after("<span class='error'>Ingrese un email válido</span>");
+	            return false;
+	        }if( $("#input-password").val() == "" || $("#input-password").val().length >= 9){
+	            $("#input-password").after("<span class='error'>Ingrese contraseña / ingrese una contrase que contenga solo números / menor de 8 dígitos</span>");
+	            return false;
+	        }else{
+        	$("#btn-inicio-sesion").attr("href","options.html")
+      	}
+
+	  }); 
+  	
+
+
    $("#button-ver-saldo").on('click',function(){
        
        var numero_tarjeta = $("#input-numero-tarjeta").val();
        
        $.ajax({
-           url: "https://bip-servicio.herokuapp.com/api/v1/solicitudes.json?bip="+numero_tarjeta,
+           url: "https://bip-servicio.herokuapp.com/api/v1/solicitudes.json?bip=" + numero_tarjeta,
+           type: 'GET',
            dataType: 'JSON',
        })
        .done( function(datos_saldo){
-       		console.log(datos_saldo);
-       		
-           
-       })
-        
+       		console.log(datos_saldo.saldoTarjeta);
+       		$("#mostrar-saldos").append(`
+                        <div  class="col s12">
+            				<div class="col s4">
+            				</div>
+            				<div class="col s4 orange white-text center-align">
+            					Saldo: `+datos_saldo.saldoTarjeta+`
+            				</div>
+            				<div class="col s4">
+            				</div>
+            			</div>
+                    `);
+       	})
+       .fail( function(){
+            alert("Sigue intentando");
+        })
     });
-   
-    
+
+
+	
 });
  
 
